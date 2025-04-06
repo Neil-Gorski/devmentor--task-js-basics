788d28bafeb830c0b1ab4a5e85d6b92b1c0ed207
Neil Gorski <neil.gorski@hotmail.ch>
2025-04-07 00:06:36 +0200

    tast done

--- js/app.js
+++ js/app.js
@@ -12,20 +12,70 @@ Calculator.prototype.getHistoryAsString = function () {
 };
 
 Calculator.prototype.add = function (num1, num2) {
-  // 1. zamień wartości przekazane przez parametr na typ number
+  const res = num1 + num2;
+  this.history.push(`${num1} + ${num2} = ${res}`);
+};
+
+Calculator.prototype.sub = function (num1, num2) {
+  const res = num1 - num2;
+  this.history.push(`${num1} - ${num2} = ${res}`);
+};
+Calculator.prototype.mul = function (num1, num2) {
+  const res = num1 * num2;
+  this.history.push(`${num1} * ${num2} = ${res}`);
+};
+
+Calculator.prototype.div = function (num1, num2) {
+  if (num2 === 0) {
+    this.history.push(`Error: Cannot divide by zero.`);
+  } else {
+    const res = num1 / num2;
+    this.history.push(`${num1} / ${num2} = ${res}`);
+  }
+};
+
+Calculator.prototype.pwr = function (num1, num2) {
+  let str = num1;
+  let res = 1;
+  let i = 1;
+  while (i < num2) {
+    str += ` * ${num1}`;
+    res *= num1;
+    i++;
+  }
+
+  this.history.push(`${str} = ${res}`);
+};
+
+Calculator.prototype.calc = function (num1, num2, action) {
   num1 = +num1;
   num2 = +num2;
-  // 2. sprawdź czy są one poprawne
+
   if (
     typeof num1 === "number" &&
     !isNaN(num1) &&
     typeof num2 === "number" &&
     !isNaN(num2)
   ) {
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    const res = num1 + num2;
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-    this.history.push(`${num1} + ${num2} = ${res}`);
+    switch (action) {
+      case "+":
+        this.add(num1, num2);
+        break;
+      case "-":
+        this.sub(num1, num2);
+        break;
+      case "*":
+        this.mul(num1, num2);
+        break;
+      case "/":
+        this.div(num1, num2);
+        break;
+      case "^":
+        this.pwr(num1, num2);
+        break;
+      default:
+        console.log("Unknown operator");
+    }
   }
 };
 
@@ -43,8 +93,6 @@ do {
     number1 = prompt("Podaj liczbę nr 1");
     number2 = prompt("Podaj liczbę nr 2");
 
-    if (action === "+") {
-      calc.add(number1, number2);
-    }
+    calc.calc(number1, number2, action);
   }
 } while (calc.isCorrectAction(action));
